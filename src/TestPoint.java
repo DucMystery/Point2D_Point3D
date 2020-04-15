e7dcb0fc6be2135f6062d591f37a4fcf6b2898b4
DucMystery <58973644+DucMystery@users.noreply.github.com>
2020-04-15 09:59:35 +0700

    Hoan thanh

---
+++ src/TestPoint.java
@@ -0,0 +1,9 @@
+public class TestPoint {
+
+    public static void main(String[] args) {
+
+        Point3D point3D1 = new Point3D(5,6,7);
+        System.out.println(point3D1.toString());
+        System.out.println(point3D1.getXYZ()[0]);
+    }
+}
